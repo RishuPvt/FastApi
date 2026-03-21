abef3ffa2c4ba25699eb2adf711b74942c8d60e2
Rishu Raj <rishuraj1127@gmail.com>
2026-03-21 12:27:07 +0530

    Added auth schema

--- Backend/routes/auth_routes.py
+++ Backend/routes/auth_routes.py
@@ -5,6 +5,8 @@ from models.user_model import User
 from utils.auth import hash_password
 from fastapi import HTTPException
 from utils.auth import verify_password, create_access_token
+from schemas.Auth_Schema import LoginRequest
+from schemas.Auth_Schema import RegisterRequest
 router = APIRouter()
 
 def get_db():
@@ -15,33 +17,46 @@ def get_db():
         db.close()
 
 
+
 @router.post("/register")
-def register(username: str, email: str, password: str, db: Session = Depends(get_db)):
+def register(data: RegisterRequest, db: Session = Depends(get_db)):
+    
+    # Check if user already exists
+    existing_user = db.query(User).filter(User.email == data.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists")
 
-    hashed_password = hash_password(password)
-
-    user = User(
-        username=username,
-        email=email,
-        password=hashed_password
+    hashed_password = hash_password(data.password)
+    # Create new user
+    new_user = User(
+        username=data.username,
+        email=data.email,
+        password=hashed_password 
     )
 
-    db.add(user)
+    db.add(new_user)
     db.commit()
-    db.refresh(user)
+    db.refresh(new_user)
 
-    return {"message": "User registered successfully"}
+    return {
+        "message": "User registered successfully",
+        "user": {
+            "id": new_user.id,
+            "email": new_user.email,
+            "username": new_user.username
+        }
+    }
 
 
 @router.post("/login")
-def login(email: str, password: str, db: Session = Depends(get_db)):
+def login(data: LoginRequest, db: Session = Depends(get_db)):
 
-    user = db.query(User).filter(User.email == email).first()
+    user = db.query(User).filter(User.email == data.email).first()
 
     if not user:
         raise HTTPException(status_code=400, detail="User not found")
 
-    if not verify_password(password, user.password):
+    if not verify_password(data.password, user.password):
         raise HTTPException(status_code=400, detail="Invalid password")
 
     token = create_access_token(
